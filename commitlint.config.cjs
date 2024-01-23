5916ea3602232f1d6d995beb61153fa31f230ec6
shasank shakya <shasank.shakya@cellcom.net.np>
2024-01-23 15:44:41 +0545

    feat(user-auth): Add new user authentication feature

---
+++ commitlint.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: [],
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'test']],
+        'type-case': [2, 'always', 'lowerCase'],
+        'type-empty': [2, 'never'],
+        'subject-case': [2, 'always', 'sentence-case'],
+        'subject-empty': [2, 'never'],
+        'scope-case': [2, 'always', 'kebab-case'],
+    },
+    plugins: [
+
+    ]
+}
