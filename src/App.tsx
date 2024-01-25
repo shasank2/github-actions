207e23618008a00a9944f09b657a9038c1da5040
shasank shakya <shasank.shakya@cellcom.net.np>
2024-01-26 04:25:45 +0545

    fix(github-actions): Just code formating

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className='read-the-docs'>
-        Click on the Vite and React logos to learn oiahuiwhq IUHu%
+        Click on the Vite and React logos to learn
       </p>
     </>
   )
