3ca5c9ef6768919d45f46295c5476ce2053f1439
shasank shakya <shasank.shakya@cellcom.net.np>
2024-01-26 04:21:39 +0545

    fix(github-actions): Testing github actions CI

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+   port: 3000,
+   strictPort: true,
+  },
+  server: {
+   port: 3000,
+   strictPort: true,
+   host: true,
+  //  origin: "http://0.0.0.0:8080",
+  },
+ });
