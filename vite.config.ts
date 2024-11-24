233f4bf79281188e0660a006bebdb2c110d045be
shasank2 <shasankshakya2@gmail.com>
2024-11-24 22:21:27 +0545

    feat(tailwind,vitest,json-server):setup for all three

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,11 @@ export default defineConfig({
   base: "/",
   plugins: [react()],
   preview: {
-   port: 3000,
+   port: 5173,
    strictPort: true,
   },
   server: {
-   port: 3000,
+   port: 5173,
    strictPort: true,
    host: true,
   //  origin: "http://0.0.0.0:8080",
