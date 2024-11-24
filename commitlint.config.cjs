9b0bf1bfb808837dbd6c3132a3552aa0cee2b8e5
shasank2 <shasankshakya2@gmail.com>
2024-11-24 23:18:25 +0545

    fix(commitlint): changes in commit lint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
         'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'test']],
         'type-case': [2, 'always', 'lowerCase'],
         'type-empty': [2, 'never'],
-        'subject-case': [2, 'always', 'sentence-case'],
+        // 'subject-case': [2, 'always', 'sentence-case'],
         'subject-empty': [2, 'never'],
         'scope-case': [2, 'always', 'kebab-case'],
     },
