4e5e636f1839071e806219cfd939884ef68a8162
shasank2 <shasankshakya2@gmail.com>
2024-11-26 05:00:44 +0545

    fix(react-router): Fixed react router provider and some tailwind styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+import axios from 'axios';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import routes from "./routes";
+
+axios.defaults.baseURL = "http://localhost:3000";
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
