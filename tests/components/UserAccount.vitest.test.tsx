f76e7054e89c863f6bfb8848a34178c54abed27d
shasank2 <shasankshakya2@gmail.com>
2024-12-02 06:57:32 +0545

    test(UserAccount & UserList): witten vitest test cases for these components

--- tests/components/UserAccount.vitest.test.tsx
+++ tests/components/UserAccount.vitest.test.tsx
@@ -10,4 +10,21 @@ describe('UserAccount', () => {
 
     expect(screen.getByText(user.name)).toBeInTheDocument();
   })
+
+  it('should render edit button if user is admin', () => {
+    const user: User = { id: 23, name: 'shasank', isAdmin: true }
+    
+    render(<UserAccount user={user} /> )
+    const button = screen.getByRole('button')
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/edit/i);
+  })
+
+  it('should not render edit button if user is not admin', () => {
+    const user: User = { id: 23, name: 'shasank', isAdmin: false }
+    
+    render(<UserAccount user={user} /> )
+    const button = screen.queryByRole('button')
+    expect(button).not.toBeInTheDocument();
+  })
 })
---
+++ tests/components/UserList.vitest.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import UserList from '../../src/components/UserList'
+import { User } from '../../src/entities'
+
+describe('UserList', () => {
+  it('should render no users when user array is empty', () => {
+    render(<UserList users={[]} />)
+    expect(screen.getByText(/no users/i)).toBeInTheDocument()
+  })
+
+  it('should render a list of users', () => {
+    const users: User[] = [
+      { id: 76, name: 'John' },
+      { id: 45, name: 'Mary' },
+    ]
+    render(<UserList users={users} />)
+    users.forEach(user => {
+      const link = screen.getByRole('link', {name:user.name});
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute('href',`/users/${user.id}`)
+    });
+  })
+})
